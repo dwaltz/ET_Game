f845ed3a50b0ef5f6e836515a039937f72468cde
dwaltz <dwaltz@exacttarget.com>
2014-03-22 18:56:05 -0400

    Turtles

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,68 @@
-requirejs.config({
-	baseUrl: '/vendor',
-	paths: {
-		// APP PATHS
-		js: '../js',
+$(function(){
+	// create an new instance of a pixi stage
+	var stage = new PIXI.Stage(0x001f8e);
 
-		'templates': '../templates',
-		// COMMON
-		'base-v': '../js/lib/base-v',
-		'base-m': '../js/lib/base-m',
-		'base-c': '../js/lib/base-c'
-	},
-	deps: [ 'js/app' ],
-	shim: {
-		'js/app': {
-			deps: [ 'backbone' ]
-		},
-		'backbone': {
-			deps: [ 'underscore', 'jquery', 'json2' ],
-			exports: 'Backbone'
-		},
-		'underscore': {
-			exports: '_'
-		},
-		'jquery': {
-			exports: '$'
+// create a renderer instance
+	var renderer = PIXI.autoDetectRenderer(800, 600);
+
+// add the renderer view element to the DOM
+	document.body.appendChild(renderer.view);
+
+	requestAnimFrame(animate);
+
+// create a texture from an image path
+	var texture = PIXI.Texture.fromImage("./img/et.png");
+	var houseS = PIXI.Texture.fromImage("./img/house.png");
+
+// create a new Sprite using the texture
+	var et = new PIXI.Sprite(texture);
+	var house = new PIXI.Sprite(houseS);
+
+// center the sprites anchor point
+	et.anchor.x = 0.5;
+	et.anchor.y = 0.5;
+	et.anchor.x = 0.5;
+	et.anchor.y = 0.5;
+
+// move the sprite t the center of the screen
+	et.position.x = 200;
+	et.position.y = 150;
+	house.position.x = 250;
+	house.position.y = 150;
+
+	stage.addChild(et);
+	stage.addChild(house);
+
+	$(window).on('keydown',function(ev){
+		switch(ev.keyCode)
+		{
+			case 37:
+				et.position.x = et.position.x - 10;
+				if( et.scale.x < 0 ) et.scale.x = 1;
+				//left
+				break;
+			case 39:
+				et.position.x = et.position.x + 10;
+				if( et.scale.x > 0 ) et.scale.x = -1;
+				//right
+				break;
+			case 38:
+				et.position.y = et.position.y - 10;
+				//down
+				break;
+			case 40:
+				et.position.y = et.position.y + 10;
+				//up
+				break;
 		}
+		animate();
+	});
+
+	function animate() {
+
+		requestAnimFrame(animate);
+
+		// render the stage
+		renderer.render(stage);
 	}
 });
